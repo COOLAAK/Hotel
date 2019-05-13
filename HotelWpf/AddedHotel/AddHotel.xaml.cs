9850d91b5cfb38238b823dbea7bef35a49f9bd5b
Bobosh_v <Bobosh_v@A33COMP1>
2019-05-13 14:06:22 +0300

    Hell

---
+++ HotelWpf/AddedHotel/AddHotel.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HotelWpf.ServiceReference1;
+
+namespace HotelWpf
+{
+    /// <summary>
+    /// Interaction logic for AddHotel.xaml
+    /// </summary>
+    public partial class AddHotel : Window
+    {
+        public AddHotel()
+        {
+            InitializeComponent();
+            using (Service1Client client = new Service1Client())
+            {
+                List
+        <Country> country = client.GetCountries().ToList();
+                foreach (var item in country)
+                {
+                    cbContr.Items.Add(item.Name);
+                }
+            }
+            cbContr.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> room = new List<int>();
+            int amount;
+            if(int.TryParse (tbAmount.Text,out amount))
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    room.Add(new int());
+                }
+            GenerateRoom generateRoom = new GenerateRoom(ref room);
+
+                generateRoom.ShowDialog();
+            }
+            MessageBox.Show("Bulka");
+        }
+    }
+}
---
+++ HotelWpf/AddedHotel/GenerateRoom.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelWpf
+{
+    /// <summary>
+    /// Interaction logic for GenerateRoom.xaml
+    /// </summary>
+    public partial class GenerateRoom : Window
+    {
+         List<int>room;
+        public GenerateRoom( ref List<int> room)
+        {
+            InitializeComponent();
+            this.room = room;
+            for (int i = 0; i < room.Count; i++)
+            {
+                StackPanel stack = new StackPanel();
+                stack.Orientation = Orientation.Horizontal;
+                
+                TextBlock tbx = new TextBlock() { Text = $"Поверх  {i+1} :" };
+                TextBox cb = new TextBox();
+                cb.Width = 100;
+                cb.Text = $"{i + 1}Працює";
+                stack.Children.Add(tbx);
+                stack.Children.Add(cb);
+
+
+                stack.Margin = new Thickness(10, 10, 0, 0);
+                DockPanel.SetDock(stack, Dock.Top);
+                dpAdd.Children.Add(stack);
+            }
+            
+         
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int countRoom=  dpAdd.Children.Count;
+            for (int i = 0; i < countRoom; i++)
+            {
+                if (dpAdd.Children[i] is StackPanel)
+                {
+                    StackPanel good = dpAdd.Children[i] as StackPanel;
+                    if (good.Children[1] is TextBox)
+                    {
+                        room[i] = int.Parse((good.Children[1] as TextBox).Text);
+                    }
+                }
+            }
+            this.Close();
+        }
+    }
+}
